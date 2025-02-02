234396d6c048db48de51aaeb836056c90c8f0c1f
justin-031 <fifarock12@gmail.com>
2025-02-02 17:52:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ const messages = [
 let messageIndex = 0;
 
 function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
+    const ñoButton = document.querySelector('.ño-button');
+    const chiButton = document.querySelector('.chi-button');
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    const currentSize = parseFloat(window.getComputedStyle(chiButton).fontSize);
+    chiButton.style.fontSize = `${currentSize * 1.5}px`;
 }
 
 function handleYesClick() {
