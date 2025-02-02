f6ce30c9967b11ac609f03e2c2bde57652a4dd09
justin-031 <fifarock12@gmail.com>
2025-02-02 17:48:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "¿Esta segura?",
+    "¿Realmente segura??",
+    "¿Si esta siendo buena?",
+    "Piesalo porfi...",
+    "Solo pienselo un momento!",
+    "Me pondré muy triste si dice que no...",
+    "Estaré muy triste...",
+    "Estare muy muy muy triste :c...",
+    "Weno, ya no preguntaré...",
+    "Es broma, Diga que chi amor! ❤️"
 ];
 
 let messageIndex = 0;
